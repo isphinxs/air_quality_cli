46cf267eb50249a32c64284cede7fab4f0a7068b
Samantha Ostrowski <isphinxs@hotmail.com>
2020-12-28 17:52:00 -0500

    Add pollutants to AirQuality class initiatlize method

--- lib/air_quality_cli/air_quality.rb
+++ lib/air_quality_cli/air_quality.rb
@@ -1,14 +1,21 @@
 class AirQuality::AirQuality
-    attr_accessor :state, :city, :station, :data
+    # attr_accessor :state, :city, :station, :data
+    attr_accessor :aqi_value, :main_pollutant
     attr_reader :country
     
     @@all = []
 
-    def initialize(state, city, station)
-        @country = "United States"
-        @state = state
-        @city = city
-        @station = station
+    # def initialize(state, city, station)
+    #     @country = "United States"
+    #     @state = state
+    #     @city = city
+    #     @station = station
+    #     save
+    # end
+
+    def initialize(aqi_value, main_pollutant)
+        @aqi_value = aqi_value
+        @main_pollutant = main_pollutant
         save
     end
 
