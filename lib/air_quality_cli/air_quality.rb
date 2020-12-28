b540187ae7589711fd1433e743e0ccd022921a1b
Samantha Ostrowski <isphinxs@hotmail.com>
2020-12-28 10:47:44 -0500

    Add initial functionality to AirQuality class

--- lib/air_quality_cli/air_quality.rb
+++ lib/air_quality_cli/air_quality.rb
@@ -2,7 +2,22 @@ class AirQuality::AirQuality
     attr_accessor :state, :city, :station, :data
     attr_reader :country
     
-    def initialize
+    @@all = []
+
+    def initialize(state, city, station)
         @country = "United States"
+        @state = state
+        @city = city
+        @station = station
+        save
     end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
 end
