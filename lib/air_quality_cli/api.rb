7c80586de6f51c096b55bfd1146b34ee9bfd4af7
Samantha Ostrowski <isphinxs@hotmail.com>
2020-12-28 17:46:51 -0500

    Add get_city and get_data methods to API class

--- lib/air_quality_cli/api.rb
+++ lib/air_quality_cli/api.rb
@@ -1,9 +1,24 @@
 class AirQuality::API
-    def self.get_data
+    def self.get_city
         key = ENV["AQI_API_KEY"]
         url = "http://api.airvisual.com/v2/cities?state=New%20York&country=USA&key=" + key
         response = RestClient.get(url)
-        response_array = JSON.parse(response)
+        response_array = JSON.parse(response)["data"]
+        response_array.each do |city|
+            puts city["city"]
+        end
         # binding.pry
     end
+
+    def self.get_data(city)
+        key = ENV["AQI_API_KEY"]
+        url = "http://api.airvisual.com/v2/city?city=" + city + "&state=New%20York&country=USA&key=" + key
+        response = RestClient.get(url)
+        response_hash = JSON.parse(response)["data"]["current"]["pollution"]
+        # response_array.each do |city|
+        #     puts city["city"]
+        # end
+        # binding.pry
+    end
+
 end
