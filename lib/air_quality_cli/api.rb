f691b9d672e056e415c0e06d220d583534a24553
Samantha Ostrowski <isphinxs@hotmail.com>
2020-12-28 17:56:08 -0500

    Link API and AirQuality classes through instantiation of AirQuality in API

--- lib/air_quality_cli/api.rb
+++ lib/air_quality_cli/api.rb
@@ -15,6 +15,10 @@ class AirQuality::API
         url = "http://api.airvisual.com/v2/city?city=" + city + "&state=New%20York&country=USA&key=" + key
         response = RestClient.get(url)
         response_hash = JSON.parse(response)["data"]["current"]["pollution"]
+        aqi_value = response_hash["aqius"]
+        main_pollutant = response_hash["mainus"]
+
+        AirQuality::AirQuality.new(aqi_value, main_pollutant)
         # response_array.each do |city|
         #     puts city["city"]
         # end
